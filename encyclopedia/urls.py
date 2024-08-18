f0ae45ec28ee40d1a446f54b6e8d6f71ee1e4e71
KenzoJ <charles.felux@gmail.com>
2024-08-17 22:28:18 -0400

    implement all pages with links

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki", views.index, name='wiki'),
+    path("wiki/<str:entry>", views.wiki, name='article'),
+    
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entry_names": util.list_entries()
     })
+
+# takes the phrase after 'wiki/' 
+    # adds it the top
+    # takes the markdown from 'entries' and passes "article" to entry.html
+
+def wiki(request, entry):
+    return render(request, "encyclopedia/entry.html", {
+        "article": markdown_convert(entry),
+    })
+
+def markdown_convert(entry):
+    single_entry = entry.capitalize()
+    article = util.get_entry(single_entry)
+    return markdown2.markdown(article)
+
 
